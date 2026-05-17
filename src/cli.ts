bcf9b8d894acf887e3466748d4d75d1b2a9b654a
Akhil Kumar <akhil@apralabs.com>
2026-05-17 02:38:59 -0400

    feat: implement help subcommand (Task 3)

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,21 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-yargs(hideBin(process.argv))
+const parser = yargs(hideBin(process.argv))
   .scriptName("tool")
   .usage("$0 <command> [args]")
   .version("fleet-e2e-toy v1.0.0")
   .alias("v", "version")
   .help()
-  .argv;
+  .alias("h", "help")
+  .command("help", "Show help", () => {}, () => {
+    parser.showHelp();
+  })
+  .command("add", "Add a new note", (y) => {
+    return y.positional("title", { describe: "Note title", type: "string" });
+  })
+  .command("serve", "Start the server", () => {})
+  .demandCommand(1, "You must provide a command");
+
+parser.argv;
 
